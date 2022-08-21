a6202c14c783c9e526521fa2998cdacb704fbcf4
Shub4am <shub4am@gmail.com>
2022-08-22 03:46:37 +0530

    Updated Thunk pattern for fetching categories

--- src/pages/CategoriesPreview/index.jsx
+++ src/pages/CategoriesPreview/index.jsx
@@ -1,18 +1,28 @@
 import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import CategoryPreview from '../../components/CategoryPreview';
-import { selectCategoriesMap } from '../../store/categories/categoriesSelector';
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from '../../store/categories/categoriesSelector';
+import Spinner from '../../components/Spinner';
+
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      )}
     </Fragment>
   );
 };
